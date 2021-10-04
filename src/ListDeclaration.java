b000ad5978f00738d0990602c54aed5851b788ac
Adnan4456 <adnan4456@gmail.com>
2021-10-04 15:41:56 +0200

    initial commit

---
+++ src/ListDeclaration.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDeclaration {
+    public static void main(String[] args) {
+
+        // List variable declarations
+        List l;
+        List<Integer> intList;
+        List<ArrayList> arrayList;
+
+        // ArrayList variable declarations
+        ArrayList a;
+        ArrayList<String> stringArray;
+        ArrayList<Integer> integerArray;
+    }
+}
